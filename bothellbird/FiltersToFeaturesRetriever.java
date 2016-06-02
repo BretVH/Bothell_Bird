49a3cb71d6975f929edb7d18cc6c795d574ea503
Bret Andrew Van Hof <bretvh@hotmail.com>
2016-06-02 13:57:49 -0700

    Spiff up Filters....

--- bothellbird/FiltersToFeaturesRetriever.java
+++ bothellbird/FiltersToFeaturesRetriever.java
@@ -5,7 +5,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -16,86 +16,84 @@ import java.util.Map;
 public class FiltersToFeaturesRetriever {
 
     private static final String[] queriesForSelectableFeatures = {
-        "SELECT * FROM BirdDatabase.dbo.Family",
-        "SELECT * FROM BirdDatabase.dbo.SecondaryColor",
-        "SELECT * FROM BirdDatabase.dbo.PrimaryColors",
+        "SELECT * FROM BirdDatabase.dbo.Locations",
         "SELECT * FROM BirdDatabase.dbo.Habitats",
-        "SELECT * FROM BirdDatabase.dbo.ConservationStatus",
         "SELECT * FROM BirdDatabase.dbo.Size",
-        "SELECT * FROM BirdDatabase.dbo.Locations"
-    };
+        "SELECT * FROM BirdDatabase.dbo.PrimaryColors",
+        "SELECT * FROM BirdDatabase.dbo.SecondaryColor",
+        "SELECT * FROM BirdDatabase.dbo.Family",
+        "SELECT * FROM BirdDatabase.dbo.ConservationStatus",};
 
     private static final String[] tableNames = {
-        "Family",
-        "SecondaryColor",
-        "PrimaryColors",
+        "Locations",
         "Habitats",
-        "ConservationStatus",
         "Size",
-        "Locations"
+        "PrimaryColors",
+        "SecondaryColor",
+        "Family",
+        "ConservationStatus"
     };
 
     public static Map<String, List<Feature>> getFilterToFeaturesMap() throws SQLException {
         int counter = 0;
-        Map<String, List<Feature>> filterToSelectableFeature = new HashMap<>();
+        Map<String, List<Feature>> filterToSelectableFeature = new LinkedHashMap<>();
         for (String query : queriesForSelectableFeatures) {
-
             String filter = tableNames[counter];
             List<Feature> feature = new ArrayList<>();
             switch (counter) {
                 case 0:
-                    feature = getFamilies(tableNames[counter], counter, query);
+                    feature = getLocations(filter, query);
                     break;
                 case 1:
-                    feature = getSecondaryColors(tableNames[counter], counter, query);
+                    feature = getHabitats(filter, query);
                     break;
                 case 2:
-                    feature = getPrimaryColors(tableNames[counter], counter, query);
+                    feature = getSizes(filter, query);
                     break;
                 case 3:
-                    feature = getHabitats(tableNames[counter], counter, query);
+                    feature = getPrimaryColors(filter, query);
                     break;
                 case 4:
-                    feature = getConservationStatuses(tableNames[counter], counter, query);
+                    feature = getSecondaryColors(filter, query);                    
                     break;
                 case 5:
-                    feature = getSizes(tableNames[counter], counter, query);
+                    feature = getFamilies(filter, query);
                     break;
                 case 6:
-                    feature = getLocations(tableNames[counter], counter, query);
+                    feature = getConservationStatuses(filter, query);
                     break;
             }
-            filterToSelectableFeature.put(tableNames[counter], feature);
+            filterToSelectableFeature.put(filter, feature);
             counter++;
         }
         return filterToSelectableFeature;
     }
 
-    private static List<Feature> getFamilies(String tn, int i, String q) throws SQLException {
+    private static List<Feature> getFamilies(String tn, String q) throws SQLException {
         return getFeatureList(tn, q, "familyName", "familyNameId");
     }
 
-    private static List<Feature> getSecondaryColors(String tn, int i, String q) throws SQLException {
+    private static List<Feature> getSecondaryColors(String tn, String q) throws SQLException {
         return getFeatureList(tn, q, "secondaryColor", "secondaryColorId");
     }
 
-    private static List<Feature> getPrimaryColors(String tn, int i, String q) throws SQLException {
+    private static List<Feature> getPrimaryColors(String tn, String q) throws SQLException {
         return getFeatureList(tn, q, "primaryColor", "primaryColorId");
     }
 
-    private static List<Feature> getHabitats(String tn, int i, String q) throws SQLException {
+    private static List<Feature> getHabitats(String tn, String q) throws SQLException {
         return getFeatureList(tn, q, "habitatNames", "habitatId");
     }
 
-    private static List<Feature> getConservationStatuses(String tn, int i, String q) throws SQLException {
+    private static List<Feature> getConservationStatuses(String tn, String q) throws SQLException {
         return getFeatureList(tn, q, "conservationStatus", "conservationStatusId");
     }
 
-    private static List<Feature> getSizes(String tn, int i, String q) throws SQLException {
+    private static List<Feature> getSizes(String tn, String q) throws SQLException {
         return getFeatureList(tn, q, "size", "sizeId");
     }
 
-    private static List<Feature> getLocations(String tn, int i, String q) throws SQLException {
+    private static List<Feature> getLocations(String tn, String q) throws SQLException {
         return getFeatureList(tn, q, "location", "locationId");
     }
 
